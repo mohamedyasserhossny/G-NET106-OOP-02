855d89c22e977d6f417768e955e23208beda1e37
mohamedyasserhossny <my226546@gmail.com>
2026-09-22 16:59:01 -0700

    Add project files.

---
+++ assigment oop2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace assigment_oop2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
