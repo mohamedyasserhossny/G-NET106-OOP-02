36bc9ead4ceaa98d439562d84e181c230457f552
mohamedyasserhossny <my226546@gmail.com>
2026-09-22 17:06:52 -0700

    question 1

--- assigment oop2/Program.cs
+++ assigment oop2/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region  question 1
+            // a) struc: value type, lightweight because it store in stack , the parmterless constractor not die when we creat one 
+            //    class: refrence type , can creat in it arrays and large thing , the parmter less constracor die when we creat one 
+            //b) because the class is refrence so it store in heap while struct value type so it store in stack 
+
+            #endregion
         }
     }
 }
